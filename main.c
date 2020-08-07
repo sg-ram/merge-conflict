885a62d76e5218b9c4af03ffd2edc10bf7a90b2b
ramanan <sramananece@gmail.com>
2020-08-07 14:13:23 +0000

    updated feature 2

--- main.c
+++ main.c
@@ -5,6 +5,13 @@ Initial Function()
   //Sample Code
 }
 
+<<<<<<< HEAD
+=======
+feaature2()
+{
+  //Sample Code for Feature 2
+}
+>>>>>>> 593012190388f0a1fa891a89fc19d4d295290a7e
 Initial Security()
 {
  //Sample Code
